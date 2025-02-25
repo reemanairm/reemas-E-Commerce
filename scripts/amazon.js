475613ab3aaec968b79082c2894811561e6d989b
reema <reemacuckoo.2005@gmail.com>
2025-02-25 11:27:11 +0530

    generate html with javascript

---
+++ scripts/amazon.js
@@ -0,0 +1,23 @@
+const products=[{
+  img:'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating:{
+    stars:4.5,
+    rating:87
+  },
+  priceCents: 1090
+},{img:'images/products/intermediate-composite-basketball.jpg',
+  name:'Intermediate Size Basketball',
+  rating:{
+    stars:4,
+    rating:127
+  },
+  priceCents:2095
+},{img:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name:'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating:{
+    stars:4.5,
+    rating:56
+  },
+  priceCents:799
+},{}]
